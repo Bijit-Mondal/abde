f59fea81777c9c0ef45905d96d9a76dee502048b
Bijit-Mondal <68532969+Bijit-Mondal@users.noreply.github.com>
2020-11-29 18:16:15 +0530

    Update youtube_search.js

--- routes/youtube_search.js
+++ routes/youtube_search.js
@@ -4,7 +4,7 @@ const {spawn} = require('child_process');
 
 var router = express.Router();
 /* TODO: Generate API KEY from https://console.developers.google.com  */
-const YOUTUBE_API_KEY = 'AIzaSyAuBqtmKlH53n2ESPUeTFWXS9YyJ_YHeNY';
+const YOUTUBE_API_KEY = 'AIzaSyCgFzYUnkAXGzJbhCin_J6Mm9VgWXPR9ko';
 
 /* GET video listing. */
 router.get('/:query', function(req, res, next) {
@@ -88,43 +88,8 @@ router.get('/download/audio/:vid/:name?', (req,res,next)=>{
 /* Send mp4 download for video_id youtube */
 router.get('/download/video/:vid/:name?', (req,res,next)=>{
     let {vid,name} = req.params;
-    vid = decodeURIComponent(vid);/*video id */
-    name = name? decodeURIComponent(name):vid;/*Optional Parameter for sending name.mp4 */
-    
-    /*Perfect CODE  */
-    try {
-            /* response attachment for triggering download instead of stream */
-            res.attachment(`${name}.mp4`);
-
-            /*Downloading ,Converting mp4 youtube video using video_id  */
-            const ytdl = spawn('youtube-dl', [
-                '-o',//output
-                '-',//stdout
-                'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',//best mp4 extension , else best
-                '--recode-video',//recode video 
-                'mp4',//to mp4 if not mp4
-                '-a',//input stream
-                '-'//stdin
-            ])
-            .on('error',(err)=>next(err))
-            .on('exit',(code)=>console.log(`Ytdl exited with code ${code}`));
-            
-            /* Setting output pipe first so that we dont lose any bits */
-            ytdl.stdout.pipe(res).on('error',(err)=>next(err));
-
-            /*Catching error on stdin */
-            ytdl.stdin.on('error',(err)=>next(err));
-
-            /* Writing video url to stdin for youtube-dl */
-            ytdl.stdin.write(`http://www.youtube.com/watch?v=${vid}`)
-
-            /*Closing the input stream; imp, else it waits */
-            ytdl.stdin.end();
-            
-
-        } catch (error) {
-            next(error);
-    }
+        vid = decodeURIComponent(vid);//video id
+            res.redirect(`http://www.9xyoutube.com/watch?v=${vid}`);
 });
 
 module.exports = router;
